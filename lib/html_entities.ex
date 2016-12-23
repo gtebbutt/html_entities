c7bf29c23285d6a54ed61588aad6e5bf3a857ed6
Sarat Chandra <saratchandra@outlook.com>
2016-12-23 18:03:22 +0530

    Update regex in decode/1

--- lib/html_entities.ex
+++ lib/html_entities.ex
@@ -26,7 +26,7 @@ defmodule HtmlEntities do
   @doc "Decode HTML entities in a string."
   @spec decode(String.t) :: String.t
   def decode(string) do
-    Regex.replace(~r/\&([^\s]+[0-9]+?)/U, string, &replace_entity/2)
+    Regex.replace(~r/\&([^\s][0-9][0-9]?[0-9]?[0-9]?)/, string, &replace_entity/2)
   end
 
   @doc "Encode HTML entities in a string."
